49239652daca8e3194879444e9ae00bb0aa6137d
naka520 <yashipipi0520@gmail.com>
2023-04-02 17:31:40 +0900

    文字入力の追加

---
+++ pages/diarymemo.tsx
@@ -0,0 +1,32 @@
+import { diaryType } from "./index";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+type Props = {
+  content: string;
+  index: number;
+  diarys: diaryType[];
+  setDiarys: (x: diaryType[]) => void;
+};
+
+const Diarymemo = ({ content }: Props) => {
+  return (
+    <Card sx={{ minWidth: 275 }} className="mt-4">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {content}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">完了</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Diarymemo;
